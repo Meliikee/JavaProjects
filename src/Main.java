2ff2757f32a3d55dbd7a178f728b7b6a19cdcc0d
Melike <dnmzmlk753@gmail.com>
2024-09-19 15:34:16 +0300

    update-3

--- src/Main.java
+++ src/Main.java
@@ -1,23 +1,24 @@
 public class Main {
     public static void main(String[] args) {
-        char grade = 'C';
-
-        switch (grade) {
-            case 'A':
-                System.out.println("Mükemmel, geçtiniz !");
-                break;
-            case 'B':
-            case 'C':
-                System.out.println("İyi, geçtiniz !");
-                break;
-            case 'D':
-                System.out.println("Fena değil, geçtiniz !");
-                break;
-            case 'F':
-                System.out.println("Malesef, kaldınız !");
-                break;
-            default:
-                System.out.println("Geçersiz harf notu girdiniz !");
+        // For döngüsü:
+        for (int i = 0; i <= 10; i += 2) {
+            System.out.println(i);
         }
+        System.out.println("For Döngüsü Bitti.");
+
+        // While döngüsü:
+        int i = 0;
+        while (i < 10) {
+            i++;
+            System.out.println(i);
+        }
+        System.out.println("While Döngüsü Bitti.");
+
+        // Do-While döngüsü:
+        int j = 1;
+        do {
+            System.out.println("Şart sağlanmasa bile bir defa çalışır.");
+        } while (j > 10);
+        System.out.println("Do-While Döngüsü Bitti.");
     }
 }
