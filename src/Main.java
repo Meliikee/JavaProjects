46b8246da2d5d666e11663c4e3b08a6a8ef4fca0
Melike <dnmzmlk753@gmail.com>
2024-09-19 15:12:09 +0300

    update-2

--- src/Main.java
+++ src/Main.java
@@ -1,17 +1,23 @@
 public class Main {
     public static void main(String[] args) {
-        int sayi1 = 20;
-        int sayi2 = 25;
-        int sayi3 = 28;
+        char grade = 'C';
 
-        int enBuyuk = sayi1;
-
-        if(enBuyuk < sayi2){
-            enBuyuk = sayi2;
+        switch (grade) {
+            case 'A':
+                System.out.println("Mükemmel, geçtiniz !");
+                break;
+            case 'B':
+            case 'C':
+                System.out.println("İyi, geçtiniz !");
+                break;
+            case 'D':
+                System.out.println("Fena değil, geçtiniz !");
+                break;
+            case 'F':
+                System.out.println("Malesef, kaldınız !");
+                break;
+            default:
+                System.out.println("Geçersiz harf notu girdiniz !");
         }
-        if (enBuyuk < sayi3) {
-            enBuyuk = sayi3;
-        }
-        System.out.println("En büyük sayı: " + enBuyuk);
     }
 }
