b873d2be9394c01795fa7bc30e32894709a2c74d
Melike <dnmzmlk753@gmail.com>
2024-09-19 18:17:10 +0300

    update-7

--- src/Main.java
+++ src/Main.java
@@ -1,34 +1,34 @@
 public class Main {
     public static void main(String[] args) {
-        String mesaj = "Bugün hava çok güzel. ";
+        int number = 2;
+        int i = 2;
 
-        System.out.println(mesaj);
-        System.out.println("5.eleman: " + mesaj.charAt(4));
-        System.out.println("Eleman sayısı: " + mesaj.length());
-        System.out.println(mesaj.concat("Yaşasın!"));
-
-        System.out.println(mesaj.startsWith("B"));
-        System.out.println(mesaj.endsWith("."));
-
-        char[] karakterler = new char[5];
-        mesaj.getChars(0, 5, karakterler, 0);
-        System.out.println(karakterler);
-
-        System.out.println(mesaj.indexOf('a')); // İlk bulduğunu verir ve aramayı bitirir.
-
-        System.out.println(mesaj.lastIndexOf('a')); // Aramaya sondan başlar ama yine index numarasını verir.
-
-        System.out.println(mesaj.replace(" ", "-"));
-        System.out.println(mesaj.substring(6));
-        System.out.println(mesaj.substring(6, 10));
-
-        for (String kelime : mesaj.split(" ")) {
-            System.out.println(kelime);
+        while (true) {
+            if (i < number || number < 2) {
+                if (number % i == 0 || number < 2) {
+                    System.out.println("Bu sayı asal değildir.");
+                    break;
+                }
+                i++;
+            } else {
+                System.out.println("Bu sayı asaldır.");
+                break;
+            }
         }
 
-        System.out.println(mesaj.toLowerCase());
-        System.out.println(mesaj.toUpperCase());
+        System.out.println("*********** OR *****************");
+        boolean isPrime = true;
 
-        System.out.println(mesaj.trim()); // Baştaki ve sondaki boşlukları atar.
+        for (int j = 2; j < number; j++) {
+            if (number % j == 0) {
+                isPrime = false;
+            }
+        }
+
+        if (isPrime == true && !(number < 2)) {
+            System.out.println("Bu sayı asaldır.");
+        } else {
+            System.out.println("Bu sayı asal değildir.");
+        }
     }
 }
