05ac382b309843b756faaae8df4c10e69565baff
Melike <dnmzmlk753@gmail.com>
2024-09-20 15:35:50 +0300

    update-14

--- src/Main.java
+++ src/Main.java
@@ -1,18 +1,10 @@
 public class Main {
     public static void main(String[] args) {
 
-        Product product = new Product();
-        product.ID = 1;
-        product.name = "Laptop";
-        product.description = "Huawei Laptop";
-        product.price = 38000.00;
-        product.stockAmount = 5;
-        System.out.println(product.name);
-        System.out.println(product.description);
-        System.out.println(product.price);
-        System.out.println(product.stockAmount);
+        DortIslem dortIslem = new DortIslem();
+        System.out.println(dortIslem.topla(2, 8));
+        System.out.println(dortIslem.topla(2, 8, 7));
+        System.out.println(dortIslem.topla(2, 8, 7, 5));
 
-        ProductManager productManager = new ProductManager();
-        productManager.Add(product);
     }
 }
--- src/Product.java
+++
@@ -1,7 +0,0 @@
-public class Product {
-    int ID;
-    String name;
-    String description;
-    double price;
-    int stockAmount;
-}
