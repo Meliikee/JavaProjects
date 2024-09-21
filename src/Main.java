9bb641dc42543ff77ab3fa5fd6cd4262aa553828
Melike <dnmzmlk753@gmail.com>
2024-09-21 21:46:07 +0300

    update-29

--- src/Main.java
+++ src/Main.java
@@ -4,8 +4,12 @@ public class Main {
         try {
             int[] sayilar = new int[]{1, 2, 3};
             System.out.println(sayilar[5]);
+        } catch (StringIndexOutOfBoundsException e) {
+            System.out.println(e);
+        } catch (ArrayIndexOutOfBoundsException e) {
+            System.out.println(e);
         } catch (Exception e) {
-            System.out.println("Hata");
+            System.out.println("Genel Hata");
             System.out.println(e);
         } finally {
             System.out.println("Ben her türlü çalışırım.");
