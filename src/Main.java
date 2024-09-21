5a159a36069cea6159bd0871d314088e634e1e1a
Melike <dnmzmlk753@gmail.com>
2024-09-21 21:30:06 +0300

    update-28

--- src/Main.java
+++ src/Main.java
@@ -1,32 +1,15 @@
-import java.util.HashMap;
-
 public class Main {
     public static void main(String[] args) {
 
-        HashMap<String, String> sozluk = new HashMap<String, String>();
-        sozluk.put("book", "kitap");
-        sozluk.put("table", "masa");
-        sozluk.put("computer", "bilgisayar");
-
-        System.out.println(sozluk);
-        System.out.println(sozluk.size());
-
-        System.out.println("---------------------");
-        for (String item : sozluk.keySet()) {
-            System.out.println(item);
+        try {
+            int[] sayilar = new int[]{1, 2, 3};
+            System.out.println(sayilar[5]);
+        } catch (Exception e) {
+            System.out.println("Hata");
+            System.out.println(e);
+        } finally {
+            System.out.println("Ben her türlü çalışırım.");
         }
 
-        for (String item : sozluk.keySet()) {
-            System.out.println("Eleman: " + item + " -> " + "Değer: " + sozluk.get(item));
-        }
-        System.out.println("---------------------");
-
-        System.out.println(sozluk.get("computer"));
-
-        System.out.println(sozluk.remove("table"));
-        System.out.println(sozluk);
-
-        sozluk.clear();
-        System.out.println(sozluk);
     }
 }
