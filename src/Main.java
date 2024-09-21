b77572ddaaf8cbdd3a494cd45d5cdd513c459558
Melike <dnmzmlk753@gmail.com>
2024-09-21 22:27:12 +0300

    update-31

--- src/Main.java
+++ src/Main.java
@@ -1,22 +1,29 @@
 import java.io.BufferedReader;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
-import java.io.IOException;
 
 public class Main {
     public static void main(String[] args) {
 
         BufferedReader reader = null;
+        int total = 0;
 
         try {
             reader = new BufferedReader(new FileReader("Sayilar.txt"));
+            String line = null;
+            while ((line = reader.readLine()) != null) {
+                total += Integer.valueOf(line);
+            }
+            System.out.println("Toplam: " + total);
         } catch (FileNotFoundException e) {
             System.out.println("Dosya bulunamadı..");
+        } catch (Exception e) {
+            e.printStackTrace();
         } finally {
             try {
                 reader.close();
-            } catch (IOException e) {
-                System.out.println("Kapatılacak dosya bulunamadı..");
+            } catch (Exception e) {
+                System.out.println("Kapatılacak dosya bulunamadı : " + e);
             }
         }
     }
