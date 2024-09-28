e2008cce980a618b26cf0da80ebf03f1675aa641
Melike <dnmzmlk753@gmail.com>
2024-09-28 16:11:39 +0300

    update-36

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,11 @@
 import java.io.File;
+import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        getFileInfo();
+        readFile();
     }
 
     public static void createFile() {
@@ -28,6 +30,20 @@ public class Main {
             System.out.println("Dosya yazılabilir mi? " + file.canWrite());
             System.out.println("Dosya okunabilir mi? " + file.canRead());
             System.out.println("Dosya boyutu (byte): " + file.length());
+        }
+    }
+
+    public static void readFile(){
+        File file = new File("C:\\Users\\dnmzm\\Files\\students.txt");
+        try {
+            Scanner reader = new Scanner(file);
+            while(reader.hasNextLine()){
+                String line = reader.nextLine();
+                System.out.println(line);
+            }
+            reader.close();
+        } catch (FileNotFoundException e) {
+            throw new RuntimeException(e);
         }
     }
 }
