4c965701bf0ef9eacafbd8d49168bee7188afd95
Melike <dnmzmlk753@gmail.com>
2024-09-28 16:01:01 +0300

    update-35

--- src/Main.java
+++ src/Main.java
@@ -3,16 +3,31 @@ import java.io.IOException;
 
 public class Main {
     public static void main(String[] args) {
+        getFileInfo();
+    }
 
+    public static void createFile() {
         File file = new File("C:\\Users\\dnmzm\\Files\\students.txt");
         try {
-            if(file.createNewFile()){
+            if (file.createNewFile()) {
                 System.out.println("Dosya oluşturuldu.");
-            }else{
+            } else {
                 System.out.println("Dosya zaten mevcut.");
             }
         } catch (IOException e) {
             throw new RuntimeException(e);
+        }
+    }
+
+    public static void getFileInfo() {
+
+        File file = new File("C:\\Users\\dnmzm\\Files\\students.txt");
+        if (file.exists()) {
+            System.out.println("Dosya adı: " + file.getName());
+            System.out.println("Dosya yolu: " + file.getAbsolutePath());
+            System.out.println("Dosya yazılabilir mi? " + file.canWrite());
+            System.out.println("Dosya okunabilir mi? " + file.canRead());
+            System.out.println("Dosya boyutu (byte): " + file.length());
         }
     }
 }
